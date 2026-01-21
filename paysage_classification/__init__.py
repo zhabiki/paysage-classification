3d41171f7cf97e5ae396599f84b8d87b7a356aa4
Владимир Чеховский <126843806+bakafox@users.noreply.github.com>
2026-01-22 00:40:41 +0700

    Update __init__.py

--- paysage_classification/__init__.py
+++ paysage_classification/__init__.py
@@ -1,16 +1,22 @@
-from .dataset.samples import read_samples
+from .dataset.samples import Sample, read_samples
 from .dataset.splitter import create_splits, read_splits
 from .inference import inference
+from .model.classifier import SceneClassifier
+from .model.preprocess import SceneDataset, process_image
 from .test import model_test, show_metrics
 from .train import model_train
 from .visualize import visualize
 
 # Хорошим тоном считается явно указать список экспорта
 __all__ = [
+    "Sample",
     "read_samples",
     "create_splits",
     "read_splits",
     "inference",
+    "SceneClassifier",
+    "SceneDataset",
+    "process_image",
     "model_test",
     "show_metrics",
     "model_train",
